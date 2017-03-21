31fcda63c37817a6f6a08b67c6362587f86013b4
Mindaugas Ažubalis <mindaugas.azubalis@gmail.com>
2017-03-21 16:25:08 +0200

    paskutinis commit, ND neuzbaigtas, deja, bet taip.

--- index.php
+++ index.php
@@ -1,22 +1,12 @@
-<?php
 
-require('autoload.php');
-
-class myClass{
-
-	public function __construct() 
-	{
-		
-		echo 'Contstruct method is called.';
-	}
-
-	public function someOtherMethod()
-	{
-		echo "The name of the TEST method from MyClass is" . __FUNCTION__ . "\n"; 
-	}
-
-	public function __destruct()
-	{
-		echo 'Destruct method is called.';
-	}
-}
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+	The test class of some dummy project
+	</title>
+</head>
+<body>
+	@yield('content')
+</body>
+</html>
---
+++ tests/firstTest.php
@@ -0,0 +1,15 @@
+<?php 
+
+
+namespace tests;
+
+
+public function testFileCanAutoloadAndConstructDestruct()
+{
+	$myClass = new \Eshop\Product\Camera;
+	$myClass->
+}
+
+
+
+ ?>
